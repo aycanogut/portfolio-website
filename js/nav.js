e995ff50d88aaad8d70e2c1d59d6f8577f8e280e
aycanogut <aycanonfire@gmail.com>
2021-06-04 23:30:58 +0300

    burger-toggler animation and nav.js functions declared

---
+++ js/nav.js
@@ -0,0 +1,17 @@
+// hamburger toggler click event
+const showNav = () => {
+  // nav toggle action
+  const navList = document.querySelector('.nav__list');
+  navList.classList.toggle('nav__show');
+
+  // burger toggler animation
+  const burgerToggler = document.querySelectorAll('.toggler');
+  const firstChild = burgerToggler[0];
+  const secondChild = burgerToggler[1];
+  const thirdChild = burgerToggler[2];
+
+  firstChild.classList.toggle('toggler-1');
+  secondChild.classList.toggle('toggler-2');
+  thirdChild.classList.toggle('toggler-3');
+
+}
