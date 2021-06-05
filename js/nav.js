49eff25808702a86dd71920e1b42e43757ac3d76
aycanogut <aycanonfire@gmail.com>
2021-06-05 13:59:11 +0300

    navbar links reset animation function declared to the nav.js

--- js/nav.js
+++ js/nav.js
@@ -1,16 +1,36 @@
 // hamburger toggler click event
+
+// nav list and links
+const navList = document.querySelector('.nav__list');
+const navLinks = document.querySelectorAll('.nav__item');
+
+// burger toggler and items
+const burgerToggler = document.querySelectorAll('.toggler');
+const firstChild = burgerToggler[0];
+const secondChild = burgerToggler[1];
+const thirdChild = burgerToggler[2];
+
+// navbar visible function
 const showNav = () => {
   // nav toggle action
-  const navList = document.querySelector('.nav__list');
   navList.classList.toggle('nav__show');
 
-  // burger toggler animation
-  const burgerToggler = document.querySelectorAll('.toggler');
-  const firstChild = burgerToggler[0];
-  const secondChild = burgerToggler[1];
-  const thirdChild = burgerToggler[2];
-
   // burger items
+  firstChild.classList.toggle('toggler-1');
+  secondChild.classList.toggle('toggler-2');
+  thirdChild.classList.toggle('toggler-3');
+}
+
+// add event to all nav links
+navLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    removeNav();
+  })
+})
+
+// reset the styles when a link clicked
+const removeNav = () => {
+  navList.classList.toggle('nav__show');
   firstChild.classList.toggle('toggler-1');
   secondChild.classList.toggle('toggler-2');
   thirdChild.classList.toggle('toggler-3');
