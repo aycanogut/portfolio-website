5ac1d656616482901a37ac542132deb2b3ad1f67
aycanogut <aycanonfire@gmail.com>
2021-06-06 13:50:36 +0300

    DOM elements defined

--- js/comment.js
+++ js/comment.js
@@ -1,6 +1,13 @@
 // take the information on contact section and append to the comment section.
 
 // 1- select the DOM elements.
+const firstName = document.querySelector('#first-name');
+const lastName = document.querySelector('#last-name');
+const userMail = document.querySelector('#e-mail');
+const userComment = document.querySelector('#message');
+const submitButton = document.querySelector('#submit');
+
+
 // 2- take that input data from the element
 // 3- concat the data
 // 4- design a new comment section inside on contact
