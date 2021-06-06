a03b5227e5649d8457ba5d101d9138fc702dbb1d
aycanogut <aycanonfire@gmail.com>
2021-06-06 13:44:47 +0300

    comment.js created in js path and directions of algorithm defined as comments

---
+++ js/comment.js
@@ -0,0 +1,9 @@
+// take the information on contact section and append to the comment section.
+
+// 1- select the DOM elements.
+// 2- take that input data from the element
+// 3- concat the data
+// 4- design a new comment section inside on contact
+// 5- define all the style and classes to comment section
+// 6- append the comments into the comment section when button is clicked
+// 7- remove the comment with 'x' button
