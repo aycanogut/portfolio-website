88a5b1a0f0b1ddbb8a0e5c21326afeceda445c9f
aycanogut <aycanonfire@gmail.com>
2021-06-06 14:16:02 +0300

    append li element to contact comments function declared

--- js/comment.js
+++ js/comment.js
@@ -6,9 +6,23 @@ const lastName = document.querySelector('#last-name');
 const userMail = document.querySelector('#e-mail');
 const userComment = document.querySelector('#message');
 const submitButton = document.querySelector('#submit');
-
+const contactList = document.querySelector('.contact__message-list');
 
 // 2- take that input data from the element
+submitButton.addEventListener('click', () => {
+  const liDOM = document.createElement('li');
+  liDOM.innerHTML = `<article>
+  <p class="comment__name">
+    ${firstName.value} ${lastName.value}
+  </p>
+  <address>${userMail.value}</address>
+  <p class="comment__message">
+    ${userComment.value}
+  </p>
+</article>`
+  contactList.append(liDOM);
+});
+
 // 3- concat the data
 // 4- design a new comment section inside on contact
 // 5- define all the style and classes to comment section
